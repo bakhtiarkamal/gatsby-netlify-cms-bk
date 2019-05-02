4abc1931d18dc710f5ddad296a4a84dc5fb457c6
Kamal <Bakhtiar.Kamal@unisys.com>
2019-05-02 13:51:19 +1000

    Footer removal and new image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,25 @@
 import React from 'react'
-import { Link } from 'gatsby'
+/* import { Link } from 'gatsby' */
 
 import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
+/* import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import vimeo from '../img/social/vimeo.svg' */
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
+      <footer className="footer has-text-black">
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
+            alt="BakhtiarKamal"
+            style={{ width: '3em', height: '4em' }}
           />
+          <p>© 2019 Bakhtiar Kamal All Rights Reserved</p>
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
+        {/* <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns">
               <div className="column is-4">
@@ -106,7 +107,7 @@ const Footer = class extends React.Component {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </footer>
     )
   }
